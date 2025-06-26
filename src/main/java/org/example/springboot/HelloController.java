12d22ef31b4c2613ccb77eeb5e0dbd93508ea413
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-06-26 15:05:40 -0400

    Practice Exercise

--- src/main/java/org/example/springboot/HelloController.java
+++ src/main/java/org/example/springboot/HelloController.java
@@ -3,11 +3,18 @@ package org.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello, Spring Boot!";
+        LocalDateTime currentDate = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = currentDate.format(formatter);
+
+        return "Hello, Spring Boot! : " + formattedDate;
     }
 }
